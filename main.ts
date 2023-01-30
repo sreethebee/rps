e9302200eb03b00280c38ee1e31cfe4c6b7c12de
sreethebee <122555167+sreethebee@users.noreply.github.com>
2023-01-30 12:18:59 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,22 @@
+function updatescore () {
+    OLED.clear()
+    OLED.writeStringNewLine("Player A;" + PA)
+    OLED.newLine()
+    OLED.writeStringNewLine("Player B;" + PB)
+    OLED.newLine()
+    OLED.writeStringNewLine("Ties;" + Ties)
+    OLED.newLine()
+    OLED.writeStringNewLine("Rounds;" + Rounds)
+}
+let Rounds = 0
+let Ties = 0
+let PB = 0
+let PA = 0
+OLED.init(128, 64)
+PA = 0
+PB = 0
+Ties = 0
+Rounds = 0
+OLED.writeStringNewLine("Do you want to play Rock Paper Scissors?")
+basic.pause(2000)
+updatescore()
